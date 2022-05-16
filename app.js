cf2b301bb3327632516ee2044c49c71944add262
Fajar Kurnia <kurniafajarkurnia@gmail.com>
2022-05-16 18:18:35 +0700

    Level 2 - Encryption and add environment variable

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const { send } = require('process');
+const encrypt = require('mongoose-encryption');
 
 
 const app = express();
@@ -14,10 +15,12 @@ app.set('view engine','ejs');
 
 mongoose.connect('mongodb://localhost:27017/userDB');
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model('User', userSchema);
 
