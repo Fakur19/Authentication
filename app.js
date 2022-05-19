29af6a282703876b036971d76123804f48286e76
Fajar Kurnia <kurniafajarkurnia@gmail.com>
2022-05-19 20:24:15 +0700

    Level 5 - Using passport.js to add cookies and session

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');  // Require bcrypt modules
-const saltRounds =10; // Set salt rounds number, 10 is sufficient, the bigger the number, the longer it will take to hash a password
+// Modules to add cookies and sessions
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 
 
 
@@ -12,19 +15,37 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+//Initial configurasion to create a session 
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Using passport in express, to start initialize passport, and initialize session
+app.use(passport.initialize());
+app.use(passport.session()); // Use pasport to manage the session 
 
 mongoose.connect('mongodb://localhost:27017/userDB');
 
+//Make new user schema with mongoose schema
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
-
+// Use passport-local-mongoose plugin to userSchema, to salt and hash password that store to database
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model('User', userSchema);
+
+//passport-local-mongoose configuration, to create local log in strategy and set a passport to serialize and deserialize
+passport.use(User.createStrategy()); // create local strategy
+passport.serializeUser(User.serializeUser()); // to serialize and fill the info/message and all identifiacation to the cookie
+passport.deserializeUser(User.deserializeUser()); // to crumble the cookie and discover the info/message inside cookie and all identification 
 
 
 //CODE HERE
@@ -40,38 +61,50 @@ app.get('/register', (req,res) => {
     res.render('register');
 });
 
+app.get('/secrets', (req,res) => {
+    if(req.isAuthenticated()){ //Check if user is authenticate or not
+        res.render('secrets'); 
+    }else{
+        res.redirect('/login'); 
+    }
+});
+
+app.get('/logout', (req,res) => {
+    // To end user session
+    req.logout();
+    res.redirect('/');
+});
+
 app.post('/register', (req,res) => {
-    bcrypt.hash(req.body.password, saltRounds, (err,hash) => {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash //using bcrypt hash function to hash password
-        });
-    
-        newUser.save((err) => {
-            if(err){
-                console.log(err);
-            }else{
-                res.render('secrets');
-            }
-        });
+    // To register data to Database
+    User.register({username: req.body.username}, req.body.password, (err,user) => {
+        if(err){
+            console.log(err);
+            res.redirect('/register');
+        }else{
+            // Authenticate username and password to database
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/secrets');
+            });
+        }
     });
 });
 
 app.post('/login', (req,res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+    // Create new user
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-    User.findOne({email: username}, (err, result) => {
+    // To authenticate user is register on DB or not using login()
+    req.login(user, (err) => {
         if(err){
             console.log(err);
         }else{
-            if(result){
-                bcrypt.compare(password, result.password, (err, result) => { // Using bcrypt to compare hashing password
-                    if(result === true){  
-                        res.render('secrets');
-                    }
-                });  
-            }
+            passport.authenticate('local')(req,res, () => {
+                res.redirect('/secrets');
+            });
         }
     });
 });
