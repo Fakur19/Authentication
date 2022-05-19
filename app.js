ac3dfc41d9069024125c34b0ae667df83e4d4fb1
Fajar Kurnia <kurniafajarkurnia@gmail.com>
2022-05-20 05:43:34 +0700

    Level 6 - OAuth 2.0 - Add Sign In with Google

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+// Module for third party authentication
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 
 
@@ -34,24 +37,61 @@ mongoose.connect('mongodb://localhost:27017/userDB');
 //Make new user schema with mongoose schema
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 // Use passport-local-mongoose plugin to userSchema, to salt and hash password that store to database
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model('User', userSchema);
 
 //passport-local-mongoose configuration, to create local log in strategy and set a passport to serialize and deserialize
 passport.use(User.createStrategy()); // create local strategy
-passport.serializeUser(User.serializeUser()); // to serialize and fill the info/message and all identifiacation to the cookie
-passport.deserializeUser(User.deserializeUser()); // to crumble the cookie and discover the info/message inside cookie and all identification 
 
+// to serialize and fill the info/message and all identifiacation to the cookie
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+}); 
+
+// to crumble the cookie and discover the info/message inside cookie and all identification
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+});  
+
+//Configure strategy on passport-google-oauth20
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 //CODE HERE
 app.get('/', (req,res) => {
     res.render('home');
 });
+
+// Get method for authenticated using google
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google', {failureRedirect: '/login'}),
+    (req,res) => {
+        res.redirect('/secrets');
+    }); 
+    
 
 app.get('/login', (req,res) => {
     res.render('login');
@@ -108,7 +148,6 @@ app.post('/login', (req,res) => {
         }
     });
 });
-
 
 
 app.listen(port, () => {
